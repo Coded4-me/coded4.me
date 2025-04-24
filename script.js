d909200f9f55cb4703cd3549ef889c6c5caef3f1
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-04-24 11:47:36 +0200

    ADD dark theme

---
+++ script.js
@@ -0,0 +1,9 @@
+const changeTheme = (theme) => {
+    const body = document.body;
+
+    if (theme === 'dark') {
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+    }
+}
