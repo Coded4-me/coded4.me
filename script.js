1643953f021991dc1d093949a56ced04f21a6090
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-05-02 11:23:43 +0200

    FIX some styles, fix responsive, add mobile navbar and animations

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-const changeTheme = (theme) => {
-    const body = document.body;
+const animatedElements = document.querySelectorAll(".animate");
 
-    if (theme === 'dark') {
-        body.classList.add('dark');
-    } else {
-        body.classList.remove('dark');
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const direction = entry.target.dataset.animateDirection || "left";
+                const translateX = direction === "left" ? [-100, 0] : [100, 0];
+                
+                anime({
+                    targets: entry.target,
+                    opacity: [0, 1],
+                    translateX: translateX,
+                    duration: 1000,
+                    easing: "easeOutExpo",
+                });
+                observer.unobserve(entry.target); // Animate only once
+            }
+        });
+    },
+    {
+        threshold: 0.1,
     }
-}
+);
+
+animatedElements.forEach((element) => {
+    observer.observe(element);
+});
+
 
 document.getElementById('contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
